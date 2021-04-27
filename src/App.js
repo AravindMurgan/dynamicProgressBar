9440a2be2e1c59cc2c49c93d1065f84a6c7d5cc8
AravindMurugan <mi.arvindmurugan@gmail.com>
2021-04-27 19:04:31 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Counter1 from "./components/Counter1";
+import Counter2 from "./components/Counter2";
+
+export default function App() {
+  return (
+    <div className="App">
+      {/* <Counter1/> */}
+      <Counter2 />
+    </div>
+  );
+}

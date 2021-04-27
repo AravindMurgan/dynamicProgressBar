7fb7439dffbd39d3c1821988e450476a30987710
AravindMurugan <mi.arvindmurugan@gmail.com>
2021-04-27 19:32:37 +0530

    bug-fixing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ProgressBar from "./progressBar/ProgressBar";
 import "./styles.css";
 
 export default function App() {
   const [value, setValue] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setValue((prevState) => {
+        let newVal = prevState + 10;
+
+        if (newVal === 100) {
+          clearInterval(interval);
+        }
+
+        return newVal;
+      });
+    }, 1000);
+  }, []);
   return (
     <div className="App">
-      <ProgressBar color="light-orange" width="300px" value={value} max="100" />
+      <ProgressBar color="#00FFFF" width="500px" value={value} max="100" />
     </div>
   );
 }
--- src/progressBar/ProgressBar.js
+++ src/progressBar/ProgressBar.js
@@ -1,15 +1,46 @@
 import React from "react";
+import PropTypes from "prop-types";
+import Styled from "styled-components";
 
-function ProgressBar() {
+const Container = Styled.div`
+   progress[value] {
+    width: ${(props) => props.width};
+
+    -webkit-appearance: none;
+    appearance: none;
+  }
+
+  progress[value]::-webkit-progress-bar {
+    height: 10px;
+    border-radius: 20px;
+    background-color: #eee;
+  }  
+
+  progress[value]::-webkit-progress-value {
+    height: 10px;
+    border-radius: 20px;
+    background-color: ${(props) => props.color};
+  }
+
+`;
+
+function ProgressBar({ color, width, value, max }) {
   return (
-    <>
+    <Container color={color} width={width}>
       <h1>React ProgressBar</h1>
-      <progress value="50" max="100" />
+      <progress value={value} max={max} />
       <br />
       <br />
-      <span>50%</span>
-    </>
+      <span></span>
+    </Container>
   );
 }
+
+ProgressBar.propTypes = {
+  color: PropTypes.string,
+  width: PropTypes.string,
+  value: PropTypes.number.isRequired,
+  max: PropTypes.number
+};
 
 export default ProgressBar;

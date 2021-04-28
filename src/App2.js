598963a11a19f9364050f2b6562ebcc655d72d85
AravindMurugan <mi.arvindmurugan@gmail.com>
2021-04-28 06:37:50 +0530

    ProgressBar2

---
+++ src/App2.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ProgressBar2 from "./progressBar/ProgressBar2";
+
+function App2() {
+  return (
+    <>
+      <h1>ProgressBar</h1>
+      <ProgressBar2 />
+    </>
+  );
+}
+
+export default App2;
---
+++ src/progressBar/ProgressBar2.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function ProgressBar2(){
+
+  return(
+    <>
+      <progress value='50' max='100'/>
+    </>
+  )
+
+
+}
+
+export default ProgressBar2

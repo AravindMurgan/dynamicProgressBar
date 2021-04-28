bf43ef2b08213945265a29a694c61f587c0be9a3
AravindMurugan <mi.arvindmurugan@gmail.com>
2021-04-28 07:40:12 +0530

    progressBar2-done

--- src/App2.js
+++ src/App2.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProgressBar2 from "./progressBar/ProgressBar2";
 
 function App2() {
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setValue((prevState) => {
+        const newValue = prevState + 10;
+
+        if (newValue === 100) {
+          clearInterval(interval);
+        }
+        return newValue;
+      });
+    }, 1000);
+  }, []);
   return (
     <>
       <h1>ProgressBar</h1>
-      <ProgressBar2 />
+      <ProgressBar2 value={value} max="100" />
     </>
   );
 }
--- src/progressBar/ProgressBar2.js
+++ src/progressBar/ProgressBar2.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
+import Styled from "styled-components";
 
+const Container = Styled.div`
+    progress[value] {
+  /* Reset the default appearance */
+  -webkit-appearance: none;
+   appearance: none;
 
-function ProgressBar2(){
+  width: 500px;
+  height: 20px;
+}
 
-  return(
-    <>
-      <progress value='50' max='100'/>
-    </>
-  )
-
+progress[value]::-webkit-progress-bar {
+  background-color: #eee;
+  marging:25px;
+  border-radius: 20px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.25) inset;
 
 }
 
-export default ProgressBar2
+progress[value]::-webkit-progress-value {
+    border-radius: 20px;
+    background-color: #1DA1F2;
+  }
+`;
+
+function ProgressBar2({ value, max }) {
+  return (
+    <Container>
+      <progress value={value} max={max} />
+      <span> {value} % </span>
+    </Container>
+  );
+}
+
+ProgressBar2.propTypes = {
+  value: PropTypes.number.isRequired,
+  max: PropTypes.string,
+  color: PropTypes.string,
+  width: PropTypes.string
+};
+
+export default ProgressBar2;

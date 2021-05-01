1ee55ed7a725a8cf7da075884d9c44861c330782
AravindMurugan <mi.arvindmurugan@gmail.com>
2021-05-01 12:19:09 +0530

    useEffect with btn

--- src/useEffect/DataFetch.js
+++ src/useEffect/DataFetch.js
@@ -1,20 +1,52 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function DataFetch() {
-  const [feeds, setFeeds] = useState([]);
+  const [post, myPost] = useState([]);
+  const [id, setId] = useState(1);
   const [loading, setLoading] = useState(true);
+  const [btn, setBtn] = useState();
 
   useEffect(() => {
     axios
-      .get("https://jsonplaceholder.typicode.com/posts/")
-      .then((res) => setFeeds(res))
+      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then((res) => myPost(res.data))
       .catch((err) => console.log(err));
     setLoading(false);
-    console.log(feeds);
-  }, []);
+    console.log(post);
+    console.log("useEffect triggered");
+  }, [id]);
 
-  return <div>Hi</div>;
+  // const myHeader= posts.map(post => (
+  //   <header key={post.id} >
+  //     <h3>{post.title}</h3>
+  //     <p>{post.body} </p>
+  //   </header>
+  // ))
+
+  // const inputHeader= id.map(item => (
+  //   <header key={item.id} >
+  //     <h3>{item.title}</h3>
+  //     <p>{item.body} </p>
+  //   </header>
+  // ))
+
+  return (
+    <div>
+      <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+      <button type="button" onClick={}>
+        FetchPost
+      </button>
+      <br />
+      <br />
+      {post.title}
+      {/* {
+       loading ? "Loading ..." : (
+         <h3>{post.title}</h3>
+       )
+      } */}
+    </div>
+  );
 }
 
 export default DataFetch;

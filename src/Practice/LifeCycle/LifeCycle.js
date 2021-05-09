5415867e7da4eed42ad72a92721406802d6c468f
AravindMurugan <mi.arvindmurugan@gmail.com>
2021-05-10 04:46:02 +0530

    practice

---
+++ src/Practice/LifeCycle/LifeCycle.js
@@ -0,0 +1,22 @@
+import React,{component} from 'react';
+import { render } from 'react-dom';
+
+Class LifeCycle extends component {
+ constructor(props){
+   super(props)
+
+   this.state={
+
+  }
+
+ }
+
+ render(){
+
+  return(
+    <div>
+      <h3>Hello React</h3>
+    </div>
+  )
+ }
+}
